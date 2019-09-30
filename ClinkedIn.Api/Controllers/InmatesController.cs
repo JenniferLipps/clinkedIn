4665534016ef38284d6b9d2222e615fb54d2a1ce
Jennifer Lipps <jjlipps@gmail.com>
2019-09-30 18:47:15 -0500

    updated singular naming for Models files and using statement

--- ClinkedIn.Api/Controllers/InmatesController.cs
+++ ClinkedIn.Api/Controllers/InmatesController.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using ClinkedIn.Api.DataAccess;
-using ClinkedIn.Api.DataAccess;
+using ClinkedIn.Api.Models;
 
 namespace ClinkedIn.Api.Controllers
 {
